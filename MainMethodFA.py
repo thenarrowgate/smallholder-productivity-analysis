fda6dca99770d4719dee29af1e8c606f846a7269
nadav12 <nadavicus@gmail.com>
2025-06-10 13:10:09 +0300

    added requirements.txt

---
+++ MainMethodFA.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+
+if __name__=="__main__":
+    pass
+    # get dataframes
+
+    # check preprocessing steps (imbalances in categorical, noise)
+    # check with noise that FAMD handles noise well (continuous, binary, ordinal, nominal)
+    # 
+
+    # run right away on them classical FAMD (with reweighting )
+
+    # 
